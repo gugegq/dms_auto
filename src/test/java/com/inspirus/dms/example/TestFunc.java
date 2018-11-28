926dd73eb518842e92604f937cbbe29c232c05c8
qiao.gu <qiao.gu@symbio.com>
2018-11-28 17:00:14 +0800

    TestFunc.java added

--- src/test/java/com/inspirus/dms/example/TestFunc.java
+++ src/test/java/com/inspirus/dms/example/TestFunc.java
@@ -3,13 +3,9 @@ package com.inspirus.dms.example;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-
 import com.inspirus.dms.basic.Browsers;
-import com.inspirus.dms.basic.ComString;
-import com.inspirus.dms.basic.CommonLocateElement;
-import com.inspirus.dms.basic.pageElements;
+import com.inspirus.dms.basic.DMS_Func;
 import com.inspirus.dms.basic.login.LoginDMS;
 
 public class TestFunc {
@@ -24,15 +20,8 @@ public class TestFunc {
 	@Test
 	public void test_func() throws Exception {
 		
-		CommonLocateElement common = new CommonLocateElement();
-		pageElements element = new pageElements();
-		ComString cstr = new ComString();
-		
 		new LoginDMS().login_dms(driver);
-		common.findCss(driver, element.recipients_btn).click();
-		common.findCss(driver, element.recipients_input).clear();
-		common.findCss(driver, element.recipients_input).sendKeys(cstr.recip_input);
-		common.findCss(driver, element.recipients_input).sendKeys(Keys.ENTER);		
+		new DMS_Func().products_search(driver, "BD706");
 
 	}
 	
